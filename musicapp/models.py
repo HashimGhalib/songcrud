6e1aa611bcc358d82cbc8b0cbe2ec90506a4f492
HashimGhalib <111003413+HashimGhalib@users.noreply.github.com>
2022-10-05 20:11:18 +0100

    initial commit

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=300, null=False)
+    last_name = models.CharField(max_length=300, null=False)
+    age = models.IntegerField(null=False)
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=300, null=False)
+    date_released = models.DateField(auto_now_add=True)
+    likes = models.IntegerField
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.SET_DEFAULT)
+
+
+class Lyric(models.Model):
+    content = models.CharField(max_length=300, null=False)
+    song_id = models.ForeignKey(Song, on_delete=models.SET_DEFAULT)
 # Create your models here.
